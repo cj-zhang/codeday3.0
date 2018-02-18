616d738b9479963179640621ec7fb4d659ca7c70
Joseph Zhang <cjzhang@uw.edu>
2018-02-17 16:30:34 -0800

    api stuff and constants

---
+++ src/components/constants.js
@@ -0,0 +1,8 @@
+export default {
+	APIS: {
+		TRAIL_URL: "https://trailapi-trailapi.p.mashape.com/",
+		TRAIL_KEY: "EOSvRzRomfmsh8sGcVOvMmh298s1p1PYaVhjsn4vlPYbif63hC",
+		IG_URL: "https://api.instagram.com/v1/",
+		IG_KEY: "13aa3633c7304759b0b788946b172d0b"
+	}
+}
